03f80040460e73237a134ce25cff718ea784ea27
samuiii <lglriwork@hotmail.com>
2023-07-10 16:58:23 +0700

    post request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/hook')
+  @Post('/hook')
   getHello(): string {
     return this.appService.getHello();
   }
